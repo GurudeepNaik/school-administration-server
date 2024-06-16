1c5e818f06bd79a45297696d629e4eb286997bdd
GurudeepNaik <gurudeepnaik1999@gmail.com>
2024-06-16 12:49:44 +0530

    init - 4

--- src/app.js
+++ src/app.js
@@ -16,8 +16,8 @@ app.use(express.static("public")); //to save/store files or images in public fol
 app.use(cookieParser()); //to access/set cookies from/in users browser
 
 // // routes import and declaration / available routes
-import adminRouter from "./routes/admin.router.js";
-app.use("/api/v1/admins", adminRouter);
+// import adminRouter from "./routes/admin.router.js";
+// app.use("/api/v1/admins", adminRouter);
 
 // import userRouter from "./routes/user.router.js";
 // app.use("/api/v1/users", userRouter);
