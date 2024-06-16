a046a789d8b3c9fe94cef83e6e94bb83aed031f4
GurudeepNaik <gurudeepnaik1999@gmail.com>
2024-06-16 12:48:14 +0530

    init - 4

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
 import express from "express";
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: process.env.CORES_ORIGIN,
-//     credentials: true,
-//   })
-// );
-// app.use(express.json({ limit: "16kb" }));
-// app.use(express.urlencoded({ extended: true, limit: "16kb" })); // telling server to understand the encoded data from an encoded url if the they are encoded
-// app.use(express.static("public")); //to save/store files or images in public folder
-// app.use(cookieParser()); //to access/set cookies from/in users browser
+app.use(
+  cors({
+    origin: process.env.CORES_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // telling server to understand the encoded data from an encoded url if the they are encoded
+app.use(express.static("public")); //to save/store files or images in public folder
+app.use(cookieParser()); //to access/set cookies from/in users browser
 
 // // routes import and declaration / available routes
-// import adminRouter from "./routes/admin.router.js";
-// app.use("/api/v1/admins", adminRouter);
+import adminRouter from "./routes/admin.router.js";
+app.use("/api/v1/admins", adminRouter);
 
 // import userRouter from "./routes/user.router.js";
 // app.use("/api/v1/users", userRouter);
